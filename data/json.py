d2ba2b36a3aa640e29df67e395ec199f64a7f8e0
Jeong-Kyu <jare92n@gmail.com>
2021-06-25 18:15:10 +0900

    210625 dataset

---
+++ data/json.py
@@ -0,0 +1,15 @@
+import os, glob
+import json
+
+
+parent_dir = os.path("C:/ai_data/task03_train/images/")
+
+print(parent_dir)
+# file_list = os.listdir(parent_dir)
+# print(file_list)
+
+# file_list_json = [file for file in file_list if file.endswith(".json")]
+
+# jsonpath = os.path.join(parent_dir, file_list_json[0])
+# with open(jsonpath) as json_file:
+#     json_data = json.load(json_file)
